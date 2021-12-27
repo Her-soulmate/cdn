4160c8f8dc6d8e725221c588fc6505c5f728dafc
Her-soulmate <51037940+Her-soulmate@users.noreply.github.com>
2021-12-27 08:53:23 +0800

    Update tv-beipy.js

--- zhaitv/js/tv-beipy.js
+++ zhaitv/js/tv-beipy.js
@@ -1,9 +1,8 @@
-
 //play点击事件
 function play() {
 	var rul = document.getElementById("url").value; //获取input链接
 	if(rul == "") {
-		alert("北漂鱼提示您：请输入链接，没连接我给你解析个毛线")
+		alert("亲爱滴请输入链接，没链接我给你解析个毛线")
 	} else {
 		var jxApi = document.getElementById("jk"); //获取选择按钮
 		var jxurl = document.getElementById("jk").selectedIndex; //获取选中的
@@ -40,12 +39,3 @@ function play() {
 		}
 	}
 }
-
-function banquan() {
-	var htmlzs = "PCEtLQpIb37jgIDimIXimIXimIXimIXimIXimIUK44CA44CA4peL4piF4piF4piF4piF4piF4piF4piF4peLICAK44CA44CA4piF4piF44CA44CA44CAIOOAgOKYheKYhSAgCuOAgOKYheKYheOAgOKIqeOAgOOAgOKIqeOAgCDimIXimIUgIArjgIDimIXimIXjgIDjgIDjgIDil4/jgIAg44CA4piF4piFICAK44CA4piF4piF44CA44CA44CA44CA44CA44CA4piF4piFICAK44CA44CA4piF4piF44CA44CA44CA44CA4piF4piFICAK44CA44CA44CA44CA4piF4piF4piF4piF44CA44CA44CA4pei4pekICAK44CA44CA4pWt44CA44Ch44Ch44Ch44Ch44CA4pWu4pWxICAK44CA44CA44CA4oCU4pSY4oCU4pSY4pSU4oCU4pSU4oCUCgnnvJblhpnvvJpCZWlweShodHRwOi8vd3d3LmJlaXB5LmNvbSkKCeeJiOadg+W9kuWxnu+8muWMl+a8gumxvAkKCeaPj+i/sO+8mkJlaXB5VGhlbWXkuLvpopjjgIIKCeaJgOacieS7o+eggeWdh+eLrOeri+e8luWGme+8jOiwoue7neS4gOWIh+W9ouW8j+eahOebl+ermeihjOS4uu+8ge+8gQotLT4="
-	var base = new Base64();
-	var result2 = base.decode(htmlzs);
-	console.log(result2);
-	$("html").before(result2);
-};
-banquan();
